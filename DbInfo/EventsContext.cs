de8ee6756942db6df5b99afe96f80ee5cfd6ab54
dusan.milenkovic <dusan.milenkovic@primeholding.com>
2017-12-21 12:35:28 +0100

    First commit

---
+++ DbInfo/EventsContext.cs
@@ -0,0 +1,18 @@
+﻿using EventsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EventsWeb.DbInfo
+{
+    public class EventsContext : DbContext
+    {
+        public EventsContext()
+            : base("EventsConnection")
+        { }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ Migrations/201712210921355_Initial.cs
@@ -0,0 +1,29 @@
+namespace EventsWeb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Events",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Location = c.String(nullable: false),
+                        StartDate = c.DateTime(nullable: false),
+                        EndDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Events");
+        }
+    }
+}
